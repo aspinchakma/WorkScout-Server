b26878a5e7c5276ee526fe34c83265db8911efa9
Aspin Chakma <aspinchakma2019@gmail.com>
2025-10-24 21:30:54 +0600

    user can delete specific their company

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
         .toArray();
       res.send(companies);
     });
+    // delete specific comapny
+    app.delete("/selectedCompanies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await companyCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
